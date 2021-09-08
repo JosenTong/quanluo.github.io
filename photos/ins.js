ad59ae6899a2659d89efb79e29d3d408e53f3f0a
quanluo <tongjue@yahoo.com>
2021-09-08 23:13:55 +0800

    Site updated: 2021-09-08 23:13:53

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/quanluo/Blog-Picture/tree/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/quanluo/Blog-Picture/tree/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/quanluo/Blog-Picture/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/quanluo/Blog-Picture/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
